1e9b939db4e0850f9fba4aa34f7648fd7bd49c8f
Teal <pershint@github.com>
2018-04-03 16:41:00 -0400

    attempt to fix cuts added to summary_tree

--- lib/utils/summary_tree.py
+++ lib/utils/summary_tree.py
@@ -15,18 +15,19 @@ def fillSumWithCuts(sum_tree,cutdict):
                             cutdict[cuttype][cut] is not None:
                         interdistcut = np.zeros(1,dtype=float64)
                         sum_tree.Branch('interdistcut', interdistcut, 'interdistcut/D')
-                        interdistcut[0] = cutdict[cuttype][cut]
+                        interdistcut[0] = float(cutdict[cuttype][cut])
                     if cut=="interevent_time" and \
                             cutdict[cuttype][cut] is not None:
                         intertimecut = np.zeros(1,dtype=float64)
                         sum_tree.Branch('intertimecut', intertimecut, 'intertimecut/D')
-                        intertimecut[0] = cutdict[cuttype][cut]
+                        intertimecut[0] = float(cutdict[cuttype][cut])
                     if cut=="r" and cutdict[cuttype][cut] is not None:
                         rcut = np.zeros(1,dtype=float64)
                         sum_tree.Branch('rcut', rcut, 'rcut/D')
-                        rcut[0] = cutdict[cuttype][cut]
+                        rcut[0] = float(cutdict[cuttype][cut]
                     if cut=="z" and cutdict[cuttype][cut] is not None:
                         zcut = np.zeros(1,dtype=float64)
                         sum_tree.Branch('zcut',zcut, 'zcut/D')
                         zcut[0] = cutdict[cuttype][cut]
+    sum_tree.Fill()
     return sum_tree
